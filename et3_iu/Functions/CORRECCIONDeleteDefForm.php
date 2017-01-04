3a5e47d5c16cbf371f1191a340ba8610dffb3b9f
ismav10 <ismavazquez@alumnos.uvigo.es>
2017-01-04 17:33:58 +0100

    Add files via upload

---
+++ et3_iu/Functions/CORRECCIONDeleteDefForm.php
@@ -0,0 +1,61 @@
+<?php
+//Formulario para el borrado de correcciones.
+$DefForm3 = array(
+	0 => array(
+	'type' => 'text',
+	'name' => 'CORRECCION_NOM',
+	'value' => '',
+	'size' => 100,
+	'required' => true,
+	'pattern' => '',
+	'validation' => '',
+	'readonly' => false
+	),
+
+	1 => array(
+		'type' => 'text',
+		'name' => 'CORRECCION_ID',
+		'value' => '',
+		'size' => 10,
+		'required' => true,
+		'pattern' => '',
+		'validation' => '',
+		'readonly' => false
+		),
+	2 => array(
+		'type' => 'text',
+		'name' => 'CORRECCION_ENTREGA',
+		'value' => '',
+		'size' => 10,
+		'required' => true,
+		'pattern' => '',
+		'validation' => '',
+		'readonly' => false
+		),
+	3 => array(
+		'type' => 'text',
+		'name' => 'CORRECCION_RUBRICA',
+		'value' => '',
+		'size' => 10,
+		'required' => true,
+		'pattern' => '',
+		'validation' => '',
+		'readonly' => false
+		),
+	4 => array(
+		'type' => 'text',
+		'name' => 'CORRECCION_NOTA',
+		'value' => '',
+		'size' => 2,
+		'required' => true,
+		'pattern' => '',
+		'validation' => '',
+		'readonly' => false
+		)
+
+
+
+	);
+
+
+?>
