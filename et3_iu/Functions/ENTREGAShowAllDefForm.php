c41bc1dfe43a2a197b582730b39808f177c393ed
cibercereal <bcgonzalez4@esei.uvigo.es>
2017-01-15 12:31:24 +0100

    Update ENTREGAShowAllDefForm.php

--- et3_iu/Functions/ENTREGAShowAllDefForm.php
+++ et3_iu/Functions/ENTREGAShowAllDefForm.php
@@ -1,5 +1,5 @@
 <?php
-$p = new ENTREGAS_Model('','','','','','','','','');
+
 //Formulario para listar los roles
 $form = array(
     0 => array(
@@ -12,60 +12,8 @@ $form = array(
         'validation' => '',
         'readonly' => false
     ),
+
     1 => array(
-        'type' => 'select',
-        'name' => 'ENTREGA_TRABAJO',
-        'value' => '',
-        'options' => $p->getIDTrab(),
-        'multiple' => $p->getIDTrab(),
-        'required' => 'true',
-        'pattern' => '',
-        'validation' => '',
-        'readonly' => false
-    ),
-    2 => array(
-        'type' => 'time',
-        'name' => 'ENTREGA_HORA',
-        'value' => '',
-        'required' => 'true',
-        'pattern' => '',
-        'validation' => '',
-        'readonly' => false
-    ),
-    3 => array(
-        'type' => 'date',
-        'name' => 'ENTREGA_FECHA',
-        'value' => '2000-01-01',
-        'min' => '1900-01-01',
-        'max' => '2200-01-01',
-        'required' => 'true',
-        'pattern' => '',
-        'validation' => '',
-        'readonly' => false
-    ),
-    4 => array(
-        'type' => 'text',
-        'name' => 'ENTREGA_ALUMNO',
-        'value' => '',
-        'size' => '',
-        'required' => 'true',
-        'pattern' => '',
-        'validation' => '',
-        'readonly' => false
-    ),
-    5 => array(
-        'type' => 'number',
-        'name' => 'ENTREGA_HORAS_DEDIC',
-        'value' => '',
-        'size' => 50,
-        'min' => 0.00,
-        'max' => 4294967295.00,
-        'required' => 'true',
-        'pattern' => '',
-        'validation' => '',
-        'readonly' => false
-    ),
-    6 => array(
         'type' => 'number',
         'name' => 'ENTREGA_ID',
         'value' => '',
@@ -76,19 +24,10 @@ $form = array(
         'pattern' => '',
         'validation' => '',
         'readonly' => false
-    ),
-    7 => array(
-        'type' => 'file',
-        'name' => 'ENTREGA_FOTO',
-        'value' => '',
-        'required' => true,
-        'pattern' => '',
-        'validation' => '',
-        'readonly' => false
     )
 
 );
-$DefForm=AñadirTipos($form);
+$DefForm=AñadirTrabajos($form);
 
 
 ?>
